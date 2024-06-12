ac06b99eb7c811c7ad147de5f88112d07e6815c8
adiatraa <adiatrahman31@gmail.com>
2024-06-12 16:32:48 +0700

    Update Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import HomePage from './pages/HomePage.jsx'
-import MoviesPage from './pages/MoviesPage.jsx'
-import WatchlistPage from './pages/WatchlistPage.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage/>,
-  },
-  {
-    path: "/movies",
-    element: <MoviesPage/>,
-  },
-  {
-    path: "/watchlist",
-    element: <WatchlistPage/>
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
